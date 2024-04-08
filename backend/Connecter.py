362b7c03f8b7f64ebe808f904390a38ba96bf2aa
Leo <CoolCat5678@users.noreply.github.com>
2024-04-08 09:57:28 +0800

    update function CreateCar, JoinCar

--- backend/Connecter.py
+++ backend/Connecter.py
@@ -3,102 +3,105 @@ from datetime import datetime
 
 
 class Connecter:
-    __conn=any
-    __cursor=any
-    
+    __conn = sqlite3.Connection
+    __cursor = sqlite3.Cursor
     def __init__(self, database: str="CoolcatDB"):
         """
-        Constructor.
-
-        Parameters:
-            database (str) deafault:CoolcatDB
+        Constructor
         """
         __dbPath = f'./database/{database}.db'
         self.__conn = sqlite3.connect(__dbPath)
         self.__cursor = self.__conn.cursor() 
         
-
-
-
     # region function
-    # 搜尋月份
-    def SearchCarMonth(self, month: int=0) -> list:
+    def SearchCarMonth(self, month: int) -> list:
         """
         This function search car in month.
-
-        Parameters:
-            month (int) range between 1 to 12
 
         return:
             list
         """
-        formattedDate = ''
-        currentDate = datetime.now()
-        
-        if month > 0 and month < 13:
-            formattedDate = '{}-{:02d}'.format(currentDate.year, month)
-        else:
-            formattedDate = currentDate.strftime('%Y-%m')
-        
-        self.__cursor.execute(f"SELECT * FROM BlackCar M WHERE M.PlannedDate LIKE '{formattedDate}%'")    
+        self.__cursor.execute(f"SELECT * FROM BlackCar M WHERE M.Month={month}")    
         rows = self.__cursor.fetchall()
         return rows
             
-    # 搜尋車名
-    def SearchCarName(self, carName: str):
+    def SearchCarName(self, carName: str, month: int) -> list:
         """
         This function search car with name.
-
-        Parameters:
-            carName (str)
+        
+        return:
+            list
         """
-        self.__cursor.execute(f"SELECT * FROM BlackCar M LEFT JOIN BlackCarPassenger D ON M.CarName=D.CarName WHERE M.CarName='{carName}'")    
+        self.__cursor.execute(f"SELECT * FROM BlackCar M LEFT JOIN BlackCarPassenger D ON M.CarName=D.CarName WHERE M.CarName='{carName}' AND M.Month={month}")
         rows = self.__cursor.fetchall()
         return rows
-                
-    # 新建車
-    def CreateCar(self, CarName: str, month: int, day: int):
+
+    def CreateCar(self, CarName: str, month: int, day: int, playerName: str, fightTime: int=60) -> bool:
         """
         This function Create a new car.
 
-        Parameters:
-            month (int) range between 1 to 12
-            day   (int) range between 1 to 31
+        return:
+            bool: success=true
         """
-        PlannedDate = ''
-        currentDate = datetime.now()
+        try:
+            date = FormattedDate(month, day)
+            self.__cursor.execute(f"INSERT INTO BlackCar (CarName, Month, PlannedDate, FightTime) VALUES ('{CarName}', {month}, '{date}', {fightTime})")
+            self.__conn.commit()
+            self.JoinCar(CarName, playerName, month)
+            return True
         
-        if 0 < month < 13:
-            PlannedDate = '{}-{:02d}-{:02d}'.format(currentDate.year, month, day)
-        else:
-            PlannedDate = currentDate.strftime('%Y-%m-%d')
-        print(PlannedDate)
-        self.__cursor.execute(f"INSERT INTO BlackCar (CarName, Month, PlannedDate) VALUES ('{CarName}', {month}, '{PlannedDate}')")
-        self.__conn.commit()
+        except Exception as e:
+            print(e)
+            return False
 
-    # def JoinCar(self):  
-    #     """
-    #     This function calculates the area of a rectangle.
+    def JoinCar(self, CarName: str, playerName: str ,month: int):  
+        """
+        This function calculates the area of a rectangle.
 
-    #     Parameters:
-    #         length (float): The length of the rectangle.
-    #         width (float): The width of the rectangle.
+        Parameters:
+            length  (float): The length of the rectangle.
+            width   (float): The width of the rectangle.
 
-    #     Returns:
-    #     """
-    #     self.__cursor.execute("INSERT INTO BlackCarPassenger (CarName, JoinNumber, PlayerName) VALUES ('CarA', 1, 'Player1')")
-    #     self.__conn.commit()
+        Returns:
+            bool: success=true
+        """
+        try:
+            joinNum = self.__cursor.execute(f"SELECT MAX(JoinNumber) FROM BlackCarPassenger WHERE CarName='{CarName}' AND Month={month}").fetchone()[0]
+            print(type(joinNum))
+            if joinNum == None:
+                joinNum = 1
+            else:
+                joinNum += 1
+            
+            self.__cursor.execute(f"INSERT INTO BlackCarPassenger (CarName, JoinNumber, PlayerName, Month) VALUES ('{CarName}', {joinNum}, '{playerName}', {month})")
+            self.__conn.commit()
+            return True
+        
+        except Exception as e:
+            print(e)
+            return False
+        
+    # def __Test(self, x):
+    #     self.__cursor.execute(x)
+    #     rows = self.__cursor.fetchone()
+    #     return rows
     # endregion
 
 
 # ----
+def FormattedDate(month: int, day: int, year: int=None) -> str:
+    formattedDate = ''
+    currentDate = datetime.now()
+    
+    if year == None:
+        formattedDate = '{}-{:02d}-{:02d}'.format(currentDate.year, month, day)
+    if year != None:
+        # TODO
+        formattedDate = '{}-{:02d}-{:02d}'.format(currentDate.year, month, day)
+    return formattedDate
 
-def coolcat():
-    return 'coolcat'
-
-
-
-x = Connecter('CoolcatDB')
-# x.CreateCar('CoolCar', 5, 15)
-# x.SearchCarMonth(5)
-print(x.CreateCar())
+# x = Connecter('CoolcatDB')
+# x.CreateCar("A", 4, 4, "coolcat")
+# x.JoinCar("A", "sadCat", 3)
+# print(x.SearchCarMonth(4))
+# print(x.SearchCarName("A", 4))
--- backend/CreateTable.py
+++ backend/CreateTable.py
@@ -21,42 +21,43 @@ CREATE TABLE BlackCar (
 cursor.execute('''
 CREATE TABLE BlackCarPassenger (
     CarName NVARCHAR(16) NOT NULL,
+    Month INT NOT NULL,
     JoinNumber INT NOT NULL,
     PlayerName NVARCHAR(16) NOT NULL,
-    DiscordID NVARCHAR(16),
+    DiscordID NVARCHAR(18),
     
-    PRIMARY KEY (CarName, JoinNumber),
-    FOREIGN KEY (CarName) REFERENCES BlackCarTable(CarName)
+    PRIMARY KEY (CarName, Month, JoinNumber),
+    FOREIGN KEY (CarName, Month) REFERENCES BlackCarTable(CarName, Month)
 )
 ''')
 
 # region test data
 # 插入 BlackCarTable 表格的數據
 cursor.execute('''
-INSERT INTO BlackCar (CarName, Month, PlannedDate) VALUES
-('Car1', 1, '2024-04-01'),
-('Car2', 2, '2024-04-05'),
-('Car3', 3, '2024-04-10')
+INSERT INTO BlackCar (CarName, Month, PlannedDate, FightTime) VALUES
+('Car1', 1, '2024-04-01', 15),
+('Car3', 2, '2024-04-05', 20),
+('Car3', 3, '2024-04-10', 16)
 ''')
 
 # 插入 BlackCarPassenger 表格的數據
-cursor.execute('''
-INSERT INTO BlackCarPassenger (CarName, JoinNumber, PlayerName, DiscordID) VALUES
-('Car1', 1, 'Player1', '123456'),
-('Car1', 2, 'Player2', '234567'),
-('Car1', 3, 'Player3', '345678'),
+# cursor.execute('''
+# INSERT INTO BlackCarPassenger (CarName, Month, JoinNumber, PlayerName, DiscordID) VALUES
+# ('Car1', 1, 1, 'Player1', '123456'),
+# ('Car1', 1, 2, 'Player2', '234567'),
+# ('Car1', 1, 3, 'Player3', '345678'),
 
-('Car2', 1, 'Player4', '456789'),
-('Car2', 2, 'Player5', '567890'),
-('Car2', 3, 'Player6', '678901'),
-('Car2', 4, 'Player7', '789012'),
+# ('Car3', 2, 1, 'Player4', '456789'),
+# ('Car3', 2, 2, 'Player5', '567890'),
+# ('Car3', 2, 3, 'Player6', '678901'),
+# ('Car3', 2, 4, 'Player7', '789012'),
 
-('Car3', 1, 'Player8', '890123'),
-('Car3', 2, 'Player9', '901234'),
-('Car3', 3, 'Player10', '012345'),
-('Car3', 4, 'Player11', '123456'),
-('Car3', 5, 'Player12', '234567')
-''')
+# ('Car3', 3, 1, 'Player8', '890123'),
+# ('Car3', 3, 2, 'Player9', '901234'),
+# ('Car3', 3, 3, 'Player10', '012345'),
+# ('Car3', 3, 4, 'Player11', '123456'),
+# ('Car3', 3, 5, 'Player12', '234567')
+# ''')
 # endregion
 
 # 提交更改並關閉連接
