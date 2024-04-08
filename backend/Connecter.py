0b47213cde18c8e0792881661fe2238d0209d5b5
Leo <CoolCat5678@users.noreply.github.com>
2024-04-08 11:44:03 +0800

    update return value as dict

--- backend/Connecter.py
+++ backend/Connecter.py
@@ -1,6 +1,12 @@
 import sqlite3
 from datetime import datetime
 
+# 更改cursor return to dict
+def DictFactory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 class Connecter:
     __conn = sqlite3.Connection
@@ -11,9 +17,9 @@ class Connecter:
         """
         __dbPath = f'./database/{database}.db'
         self.__conn = sqlite3.connect(__dbPath)
+        self.__conn.row_factory = DictFactory
         self.__cursor = self.__conn.cursor() 
         
-    # region function
     def SearchCarMonth(self, month: int) -> list:
         """
         This function search car in month.
@@ -32,11 +38,11 @@ class Connecter:
         return:
             list
         """
-        self.__cursor.execute(f"SELECT * FROM BlackCar M LEFT JOIN BlackCarPassenger D ON M.CarName=D.CarName WHERE M.CarName='{carName}' AND M.Month={month}")
+        self.__cursor.execute(f"SELECT * FROM BlackCar M LEFT JOIN BlackCarPassenger D ON M.CarName=D.CarName AND M.Month=D.Month WHERE M.CarName='{carName}' AND M.Month={month}")
         rows = self.__cursor.fetchall()
         return rows
 
-    def CreateCar(self, CarName: str, month: int, day: int, playerName: str, fightTime: int=60) -> bool:
+    def CreateCar(self, CarName: str, month: int, day: int, playerName: str,discordID = str, fightTime: int=60) -> bool:
         """
         This function Create a new car.
 
@@ -45,16 +51,16 @@ class Connecter:
         """
         try:
             date = FormattedDate(month, day)
-            self.__cursor.execute(f"INSERT INTO BlackCar (CarName, Month, PlannedDate, FightTime) VALUES ('{CarName}', {month}, '{date}', {fightTime})")
+            self.__cursor.execute(f"INSERT INTO BlackCar (CarName, Month, PlannedDate, FightTime, DiscordID) VALUES ('{CarName}', {month}, '{date}', {fightTime}, {discordID})")
             self.__conn.commit()
-            self.JoinCar(CarName, playerName, month)
+            self.JoinCar(CarName, playerName, month, discordID)
             return True
         
         except Exception as e:
             print(e)
             return False
 
-    def JoinCar(self, CarName: str, playerName: str ,month: int):  
+    def JoinCar(self, CarName: str, month: int,playerName: str , discordID = str):  
         """
         This function calculates the area of a rectangle.
 
@@ -66,14 +72,28 @@ class Connecter:
             bool: success=true
         """
         try:
-            joinNum = self.__cursor.execute(f"SELECT MAX(JoinNumber) FROM BlackCarPassenger WHERE CarName='{CarName}' AND Month={month}").fetchone()[0]
-            print(type(joinNum))
+            # 檢查是否有車
+            if(self.__cursor.execute(f"SELECT 1 FROM BlackCar WHERE CarName='{CarName}' AND Month={month} AND Finished='N'").fetchone()):
+                pass
+            else:
+                print("Car Not Found")
+                return False
+            
+            # 檢查是否加入過
+            if(self.__cursor.execute(f"SELECT 1 FROM BlackCarPassenger WHERE CarName='{CarName}' AND Month={month} AND PlayerName='{playerName}'").fetchone() == None):
+                pass
+            else:
+                print("You Have Joined")
+                return False
+            
+            # 加入順序+1
+            joinNum = self.__cursor.execute(f"SELECT MAX(JoinNumber) FROM BlackCarPassenger WHERE CarName='{CarName}' AND Month={month}").fetchone()["MAX(JoinNumber)"]
             if joinNum == None:
                 joinNum = 1
             else:
                 joinNum += 1
             
-            self.__cursor.execute(f"INSERT INTO BlackCarPassenger (CarName, JoinNumber, PlayerName, Month) VALUES ('{CarName}', {joinNum}, '{playerName}', {month})")
+            self.__cursor.execute(f"INSERT INTO BlackCarPassenger (CarName, JoinNumber, PlayerName, Month, DiscordID) VALUES ('{CarName}', {joinNum}, '{playerName}', {month}, '{discordID}')")
             self.__conn.commit()
             return True
         
@@ -81,14 +101,13 @@ class Connecter:
             print(e)
             return False
         
-    # def __Test(self, x):
-    #     self.__cursor.execute(x)
-    #     rows = self.__cursor.fetchone()
-    #     return rows
-    # endregion
+    def _Test(self, x):
+        self.__cursor.execute(x)
+        rows = self.__cursor.fetchall()
+        return rows
 
 
-# ----
+
 def FormattedDate(month: int, day: int, year: int=None) -> str:
     formattedDate = ''
     currentDate = datetime.now()
@@ -100,8 +119,16 @@ def FormattedDate(month: int, day: int, year: int=None) -> str:
         formattedDate = '{}-{:02d}-{:02d}'.format(currentDate.year, month, day)
     return formattedDate
 
-# x = Connecter('CoolcatDB')
-# x.CreateCar("A", 4, 4, "coolcat")
-# x.JoinCar("A", "sadCat", 3)
-# print(x.SearchCarMonth(4))
-# print(x.SearchCarName("A", 4))
+
+# test -----------------------------------------------------------------
+
+x = Connecter('CoolcatDB')
+x.JoinCar("A", 4, "mwcat", "cat7272727")
+# y = x._Test("SELECT * FROM BlackCar")
+# for i in y:
+#     print(i)
+    
+# y = x._Test("SELECT * FROM BlackCarPassenger")
+# for i in y:
+#     print(i)
+    
--- backend/CreateTable.py
+++ backend/CreateTable.py
@@ -11,6 +11,7 @@ CREATE TABLE BlackCar (
     Month INT NOT NULL,
     Finished CHAR(1) DEFAULT 'N' NOT NULL,
     PlannedDate DATETIME,
+    DiscordID NVARCHAR(18),
     FightTime INT,
     
     PRIMARY KEY (CarName, Month)
@@ -31,34 +32,31 @@ CREATE TABLE BlackCarPassenger (
 )
 ''')
 
-# region test data
-# 插入 BlackCarTable 表格的數據
-cursor.execute('''
-INSERT INTO BlackCar (CarName, Month, PlannedDate, FightTime) VALUES
-('Car1', 1, '2024-04-01', 15),
-('Car3', 2, '2024-04-05', 20),
-('Car3', 3, '2024-04-10', 16)
-''')
+# # region test data
+# # 插入 BlackCarTable 表格的數據
+# cursor.execute('''
+# INSERT INTO BlackCar (CarName, Month, PlannedDate, FightTime, DiscordID) VALUES
+# ('Car1', 1, '2024-04-01', 15, '123456'),
+# ('Car3', 2, '2024-04-05', 20, '123456'),
+# ('Car3', 3, '2024-04-10', 16, '123456')
+# ''')
 
-# 插入 BlackCarPassenger 表格的數據
+# # 插入 BlackCarPassenger 表格的數據
 # cursor.execute('''
 # INSERT INTO BlackCarPassenger (CarName, Month, JoinNumber, PlayerName, DiscordID) VALUES
-# ('Car1', 1, 1, 'Player1', '123456'),
 # ('Car1', 1, 2, 'Player2', '234567'),
 # ('Car1', 1, 3, 'Player3', '345678'),
 
-# ('Car3', 2, 1, 'Player4', '456789'),
 # ('Car3', 2, 2, 'Player5', '567890'),
 # ('Car3', 2, 3, 'Player6', '678901'),
 # ('Car3', 2, 4, 'Player7', '789012'),
 
-# ('Car3', 3, 1, 'Player8', '890123'),
 # ('Car3', 3, 2, 'Player9', '901234'),
 # ('Car3', 3, 3, 'Player10', '012345'),
 # ('Car3', 3, 4, 'Player11', '123456'),
 # ('Car3', 3, 5, 'Player12', '234567')
 # ''')
-# endregion
+# # endregion
 
 # 提交更改並關閉連接
 conn.commit()
