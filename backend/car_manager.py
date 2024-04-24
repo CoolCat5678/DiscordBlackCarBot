de2db32dffc0a9301b46d0116d05ec59827e328e
Leo <CoolCat5678@users.noreply.github.com>
2024-04-24 17:15:01 +0800

    update car_manager

--- backend/car_manager.py
+++ backend/car_manager.py
@@ -3,23 +3,30 @@ from connecter import conn
 
 class CarManager:
     def __init__(self) -> None:
-        self.cars = None
+        self.car_list = CarList()
         pass
     
     def delete_car(self, car: Car):
         pass
-     
+    
     def update_car(self, car: Car):
+        pass
+    
+    def delete_passenger(self, car: Car, passenger: Passenger):
+        pass
+    
+    def add_passenger(self, car: Car, passenger: Passenger):
         pass
     
     def search_car(self, month) -> CarList:
         data = conn.search_car_month(month)
-        self.cars = CarList(data)
-
+        self.car_list._create_list(data)
     
 def main():
     cm = CarManager()
     cm.search_car(4)
+    print(cm.car_list['懷特車'])
+
     
 if __name__=='__main__':
     main()
--- backend/car_model.py
+++ backend/car_model.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 class Passenger:
     def __init__(self, data) -> None:
         self.JoinNumber = data['JoinNumber']
@@ -5,11 +7,15 @@ class Passenger:
         self.DiscordID = data['DiscordID']
         
     def __repr__(self) -> str:
-        return(f'JoinNumber:{self.JoinNumber}, PlayerName:{self.PlayerName}, DiscordID:{self.DiscordID}')
+        result = "\n"
+        result += f"JoinNumber : {self.JoinNumber}\n"
+        result += f"PlayerName : {self.PlayerName}\n"
+        result += f"DiscordID : {self.DiscordID}\n"
+        return result
 
 class Car:
     def __init__(self, data) -> None:
-        self._passengers = {}
+        self.__passengers: Dict[int, Passenger] = {}
         self.CarName: str = data['CarName']
         self.Month: int = int(data['Month'])
         self.Finished = data['Finished']
@@ -18,51 +24,59 @@ class Car:
         self.FightTime: int = int(data['FightTime'])
     
     def join_passenger(self, passenger: Passenger):
-        self._passengers[passenger.JoinNumber] = passenger
+        self.__passengers[passenger.JoinNumber] = passenger
     
     def __repr__(self) -> str:
-        result = ""
-        for key, value in self._passengers.items():
-            result += f"{key}: {value}\n"
+        result = "\n"
+        result += f"CarName : {self.CarName}\n"
+        result += f"Month : {self.Month}\n"
+        result += f"Finished : {self.Finished}\n"
+        result += f"PlannedDate : {self.PlannedDate}\n"
+        result += f"DiscordID : {self.DiscordID}\n"
+        result += f"FightTime : {self.FightTime}\n"
+        num_list = []
+        for i in self.__passengers.values():
+            num_list.append(i.JoinNumber)
+        result += f"num_list : {str(num_list)}\n"
         return result
     
     def __iter__(self):
-        self._passenger_iter = iter(self._passengers.values())
+        self._passenger_iter = iter(self.__passengers.values())
         return self
     
     def __next__(self):
         return next(self._passenger_iter)
     
     def __getitem__(self, key) -> Passenger:
-        return self._passengers[key]
+        return self.__passengers[key]
     
     def __len__(self):
-        return len(self._passengers)
+        return len(self.__passengers)
     
 
 class CarList:
-    def __init__(self, datas) -> None:
-        self._cars = {}
-        self.create_list(datas)
-            
-    def create_list(self, datas):
-        for data in datas:
-            if data['CarName'] not in self._cars:
-                self._cars[data['CarName']] = Car(data)
-            self._cars[data['CarName']].join_passenger(Passenger(data))
+    def __init__(self) -> None:
+        self.__cars: Dict[str, Car] = {}
             
     def __repr__(self) -> str:
-        result = ""
-        for key, value in self._cars.items():
-            result += f"{key}: {len(value)}人\n"
+        result = "\n"
+        for key, value in self.__cars.items():
+            result += f"{key}:{len(value)}人 預計:{value.PlannedDate}\n"
         return result
     
     def __iter__(self):
-        self._passenger_iter = iter(self._cars.values())
+        self._passenger_iter = iter(self.__cars.values())
         return self
 
     def __next__(self):
         return next(self._passenger_iter)
     
     def __getitem__(self, key) -> Car:
-        return self._cars[key]
+        return self.__cars[key]
+    
+    def _create_list(self, datas):
+        for data in datas:
+            if data['CarName'] not in self.__cars:
+                self.__cars[data['CarName']] = Car(data)
+            self.__cars[data['CarName']].join_passenger(Passenger(data))
+            
