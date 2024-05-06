07984e38399b2cbf8b4a2d7dbc37e2841c4a7383
Whiterzi <catwhiter@gmail.com>
2024-05-06 12:05:00 +0800

    refine modal, pass db data to detail_list

--- bot/model/bot_model.py
+++ bot/model/bot_model.py
@@ -1,12 +1,12 @@
-from .car_model import Car
-# from backend.car_manager import CarManager
+from .car_model import Car, CarList, Passenger
+from backend.car_manager import CarManager
 from typing import Generator
 
 class BotModel:
   __instance = None
 
   def __init__(self):
-    # self.manager = CarManager()
+    self.manager = CarManager()
     pass
 
   @staticmethod
@@ -17,24 +17,27 @@ class BotModel:
   
    ## wait for manager ready
   def search_car_month(self, month: int, load_limit: int = 3) -> Generator[list, None, None]:
-    car_list = [f"car{i}" for i in range(10)]
+    car_list = self.manager.get_car_list(Month=4)
     return self.car_lazy_loader(target=car_list, load_limit=load_limit)
+  
+  def search_car_name(self, car_name: str) -> list:
+    car_list = self.manager.get_car_list(CarName=car_name)
+    return self.car_lazy_loader(target=car_list)
+  
+  # def join_car(self, car_name: str, month: int, player_name: str, discord_id: str) -> bool:
+  #   return True
 
   # def search_car_name(self, car_name: str, month: int) -> list:
   #   return conn.search(CarName=car_name, Month=month)
   
   # def create_car(self, car_name: str, month: int, day: int, player_name: str, discord_id = str, fight_time: int=60) -> bool:
-  #   return conn.create_car(car_name, month, day, player_name, discord_id, fight_time)
+  #   return True
   
-  # def join_car(self, car_name: str, month: int, player_name: str, discord_id = str) -> bool:
-  #   return conn.join_car(car_name, month, player_name, discord_id)
-  
-  def car_lazy_loader(self, target: list,load_limit: int = 3) -> Generator[list, None, None]:
+  def car_lazy_loader(self, target: CarList,load_limit: int = 3) -> Generator[list, None, None]:
     temp = []
     try: 
       for carData in target:
         temp.append(carData)
-        # temp.append(Car(carData))
         if len(temp) == load_limit:
           yield temp
           temp.clear()
